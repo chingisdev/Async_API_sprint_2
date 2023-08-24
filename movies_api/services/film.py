3d1fedaae1d135a7ef58bb54aae8a28b5ffd162a
Chingis <chingisdev@gmail.com>
2023-08-25 01:13:20 +0300

    Apply composition over inheritance

--- movies_api/services/film.py
+++ movies_api/services/film.py
@@ -8,62 +8,39 @@ from fastapi import Depends
 from models.film import Film
 from search_engine.search_engine_protocol import SearchEngineProtocol
 
-from .cachable_service import FilmCachableService
+from .cachable_service import FilmCachingService
 from .elastic_service import FilmElasticService
 
 
-class FilmService(FilmCachableService):
-    redis_prefix_single = "movie"
-    redis_prefix_plural = "movies"
-
-    def __init__(
-        self,
-        redis: CacheStorageProtocol,
-        elastic: SearchEngineProtocol,
-    ):
-        self.redis = redis
+class FilmService:
+    def __init__(self, redis: CacheStorageProtocol, elastic: SearchEngineProtocol):
+        self.redis = FilmCachingService(redis=redis, prefix_plural="movies", prefix_single="movie")
         self.elastic = FilmElasticService(elastic=elastic, index="movies")
 
     async def get_by_id(self, film_id: str) -> Optional[Film]:
-        film = await self._get_instance_from_cache(film_id)
+        film = await self.redis.get_instance_from_cache(film_id)
         if not film:
             film = await self.elastic.get_by_id(film_id)
             if not film:
                 return None
-            await self._put_instance_to_cache(film)
-
+            await self.redis.put_instance_to_cache(film)
         return film
 
     async def get_by_parameters(
-        self,
-        page_number: int,
-        page_size: int,
-        search: str | None = None,
-        sort: str | None = None,
+        self, page_number: int, page_size: int, search: str | None = None, sort: str | None = None
     ) -> list[Optional[Film]]:
-        films = await self._get_list_from_cache(
-            search=search,
-            page_size=page_size,
-            page_number=page_number,
-            sort=sort,
+        films = await self.redis.get_list_from_cache(
+            search=search, page_size=page_size, page_number=page_number, sort=sort
         )
         if not films:
             films = await self.elastic.get_by_parameters(
-                search=search,
-                page_number=page_number,
-                page_size=page_size,
-                sort=sort,
+                search=search, page_number=page_number, page_size=page_size, sort=sort
             )
             if not films:
                 return []
-            await self._put_list_to_cache(
-                search=search,
-                page_number=page_number,
-                page_size=page_size,
-                sort=sort,
-                instances=films,
+            await self.redis.put_list_to_cache(
+                search=search, page_number=page_number, page_size=page_size, sort=sort, instances=films
             )
-
         return films
 
 
