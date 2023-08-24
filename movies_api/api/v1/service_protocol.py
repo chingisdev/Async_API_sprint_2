878628586e7c826fe75b9163b7b073ed30d28869
Chingis <chingisdev@gmail.com>
2023-08-24 18:32:31 +0300

    Add protocol for model services

---
+++ movies_api/api/v1/service_protocol.py
@@ -0,0 +1,15 @@
+from typing import List, Optional, Protocol, TypeVar
+
+from pydantic import BaseModel
+
+T = TypeVar("T", bound=BaseModel)
+
+
+class ModelServiceProtocol(Protocol[T]):
+    async def get_by_id(self, model_id: str) -> Optional[T]:
+        ...
+
+    async def get_by_parameters(
+        self, search: Optional[str], page_number: int, page_size: int, sort: str = None
+    ) -> List[Optional[T]]:
+        ...
